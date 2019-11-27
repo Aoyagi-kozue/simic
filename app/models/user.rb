b56c4ae9814d154efbfa44c5cea330a6aa1c7ec0
Aoyagi-kozue <sbrn.chtsk@icloud.com>
2019-11-27 15:30:54 +0000

    [Delete] 不要になったファイルを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,6 @@ class User < ApplicationRecord
   has_many :comic, dependent: :destroy
 
   has_many :reviews
-  has_many :comics, through: :mylists
-  has_many :comics, through: :reads
-  has_many :comics, through: :recommends
-  has_many :comics, through: :evaluations
   has_many :comics, through: :reviews
 
 end
